d8701ba58c4ff59d41683122213cc8526e21ae0f
Nick Meccia <nickmeccia@gmail.com>
2011-09-21 10:16:52 -0500

    started train schedule

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Markup::Application.config.secret_token = 'fae7c3abe0f9c13bd0a4221581ed82917eb0590628e16ff4d22226892b5f95f19691a409d70305476d704c976b5b4269f3404f2b7dc43ff144c502ec02c77e1a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Markup::Application.config.session_store :cookie_store, key: '_markup_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Markup::Application.config.session_store :active_record_store
